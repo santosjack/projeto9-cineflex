e63741d154db4f633ffc31990416665803049887
santosjack <jacksantos1408@gmail.com>
2022-08-01 03:04:33 -0300

    Funcionalidade Reservar assentos: ok

--- src/components/Session/Session.js
+++ src/components/Session/Session.js
@@ -24,31 +24,80 @@ export default function Session ({props}) {
         );
     },[ticket.isConfirmed]);
 
-    function Seat(obj){
-        const [selected, setSelected] = useState(false);
-        function select (e) {
-            console.log(e.target)
-            if(e.target.value.isAvailable){
-                setSelected(!select);
-                let newSeats = ticket.seats;
-                if(ticket.seats.includes(obj)){
-                    newSeats = ticket.seats.filter((item) => {
-                        if(item.id !== obj.id){
-                            return item;
-                        }
-                    });
-                }else{
-                    setTicket({...ticket, seats: newSeats});
-                }
-                
-            }
+   function setSeat (seat) {
+    seat.setSelected(!seat.selected);
+    if(seat.obj.isAvailable){
+        let newSeats = [];
+   
+        newSeats = ticket.seats.filter( item => item !== seat.obj.id );
+        console.log("valor da new: "+newSeats);
+        if(newSeats.length < ticket.seats.length){
+            setTicket({...ticket, seats: newSeats});
+        }else{
+            setTicket({...ticket, seats: [...ticket.seats, seat.obj.id]});
         }
+    
+    }
+    console.log(ticket.seats);
+   }
+
+    function Seat(obj){
+        const [ selected, setSelected ] = useState(false);
+
         return(
             <div 
             className={`seat ${obj.isAvailable ? (selected ? "green" : "gray"): "yellow"}`} 
-            onClick={() => select}>
+            onClick={() => setSeat({obj, setSelected, selected})}>
                 {obj.name}
             </div>
+        )
+    }
+
+    function Form () {
+        const [name, setName ] = useState("");
+        const [cpf, setCpf ] = useState("");
+
+        function finish(e){
+            e.preventDefault();
+            if(ticket.seats.length === 0){
+                alert("Selecione pelo menos 1 assento!");
+            }else{
+                console.log(name, cpf, ticket.seats.length);
+
+                
+                    const resp = axios.post(`https://mock-api.driven.com.br/api/v7/cineflex/seats/book-many`, {
+                        ids: ticket.seats,
+                        name: name,
+                        cpf: cpf
+                    });
+            
+                    resp.then( r => {
+                        console.log(r.statusText, r.data)
+                    }
+                    ).catch();
+            
+                
+            }
+            
+        }
+
+        return(
+            <form onSubmit={finish}>
+                <div>
+                    <label htmlFor="name">Nome do comprador: </label>
+                    <input type="text" name="name" onChange={e => setName(e.target.value)} value={name} required placeholder='Digite seu nome...'/>
+                </div>
+                <div>
+                    <label htmlFor="cpf">CPF do comprador: </label>
+                    <input type="number" name="cpf" onChange={e => setCpf(e.target.value)} value={cpf} required placeholder='Digite seu CPF...'/>
+                </div>
+                <div>
+                <input type="number" name="seats" defaultValue={ticket.seats.length} required hidden />
+                </div>
+                <div>
+                    <button type="submit">Reservar assento(s)</button>
+                </div>
+            </form>
         )
     }
 
@@ -69,7 +118,25 @@ export default function Session ({props}) {
                       />
                     )
                 )}
+                <div className='caption'>
+                    <div>
+                        <div className='seat green'></div>
+                        <p>Selecionado</p>
+                    </div>
+                    <div>
+                        <div className='seat gray'></div>
+                        <p>Disponível</p>
+                    </div>
+                    <div>
+                        <div className='seat yellow'></div>
+                        <p>Indisponível</p>
+                    </div>
+                </div>
             </div>
+            <div className='form'>
+                    <Form />
+            </div>
+
         </div>
     )
 }
